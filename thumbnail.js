9cc6fc69654f8f9eb4d15d95c037da9ae7dea0e6
a1exth3great <alex_s_mocanu@yahoo.com>
2026-03-05 13:05:34 -0600

    .

--- thumbnail.js
+++ thumbnail.js
@@ -1,5 +1,9 @@
 const bgImage = new Image();
 bgImage.src = 'assets/images/BSSModdedThumbnail (1).png'; 
 
+// This function waits for the image to be fully ready
+bgImage.onload = function() {
+    ctx.drawImage(bgImage, 0, 0, 1920, 1080);
+    // Put any code for BUTTONS here so they draw ON TOP of the background
+};
 
-ctx.drawImage(bgImage, 0, 0, 1920, 1080); 
