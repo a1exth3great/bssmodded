bbabdcd2d4f9278e6f3f35e5c21f89960e7c7287
a1exth3great <alex_s_mocanu@yahoo.com>
2026-03-05 11:32:15 -0600

    changed image

--- thumbnail.js
+++ thumbnail.js
@@ -1,1 +1,5 @@
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU-52HmgfPBfyCxaChR_eWSCUzbtLe5cBjhA&amp;s" alt="🤓 Nerd Face Emoji: Meaning &amp; Usage"/>
+const bgImage = new Image();
+bgImage.src = 'assets/images/background.png'; // Path to your new file
+
+// Inside the game's "draw" or "render" function:
+ctx.drawImage(bgImage, 0, 0, 1920, 1080); 
